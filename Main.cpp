b878f6670e904e4180b1dc73b227306dc075d08e
JasonDeras <derasjason2@gmail.com>
2020-03-13 14:08:45 -0600

    Segundo update del Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -3,13 +3,58 @@
 #include"Complejo.h"
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 //Vector de numeros
 vector<Racional> *lista_Racional;
 
-using namespace std;
+int menu(){
+	
+	int opcion;
+
+	cout<<"Menu"<<endl;
+	cout<<"1. Crear Vector"<<endl;
+	cout<<"2. Listar Vectores"<<endl;
+	cout<<"3. Operar vectores"<<endl;
+	cout<<"Ingrese una opcion: ";
+	cin>>opcion;
+	cout<<endl<<enl;
+
+	return opcion;
+
+}//Fin del metodo menu 
 
 int main(){
+
+	int usuario=1;
+
+	while(usuario==1){
+
+		switch(menu()){
+			
+			case 1:{
+			break;}
+
+
+			case 2:{
+			break;}
+
+			case 3:{
+			break;}
+
+			default:{
+				cout<<"Opcion no valida"<<endl<<emdl;
+			break;}	
+
+		}//Fin de las opciones del menu
+
+		cout<<"Volver al menu [1.-Si/2.-No]: ";
+		cin>>usuairo;
+		cout<<endl<<endl;
+	
+	}//fin del while del usuario;
+	
 	return 0;
+
 }//Fin del main 
