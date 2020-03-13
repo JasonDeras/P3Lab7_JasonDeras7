00440cfeaa7e65aa6c57768e4ed8f32fb4e2610f
JasonDeras <derasjason2@gmail.com>
2020-03-13 14:01:54 -0600

    Update del main

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+#include"Vector.h"
+#include"Racional.h"
+#include"Complejo.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//Vector de numeros
+vector<Racional> *lista_Racional;
+
+using namespace std;
+
+int main(){
+	return 0;
+}//Fin del main 
